50e4fc2d5c3897c6ac14f4fc2fb535e857bd3a47
08sg08 <shashwatg24@iimb.ac.in>
2025-04-13 16:54:22 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+const watches = [
+  {
+    brand: "Tissot",
+    model: "PRX Powermatic 80",
+    reference: "T1374071104100",
+    price: 695,
+    currency: "USD",
+    color: "#000000",
+    link: "https://www.ethoswatches.com/tissot-prx-powermatic-80-t1374071104100.html",
+    seller: "Ethos Watches",
+    logo: "https://www.ethoswatches.com/pub/media/favicon/stores/1/favicon-96x96.png",
+    inStock: true,
+    lastDrop: "Lowest in 30 days!"
+  },
+  {
+    brand: "Rolex",
+    model: "Submariner Date",
+    reference: "126610LN",
+    price: 12500,
+    currency: "USD",
+    color: "#004225",
+    link: "https://www.rolex.com/watches/submariner/m126610ln-0001.html",
+    seller: "Rolex Official",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Rolex_logo.svg/1024px-Rolex_logo.svg.png",
+    inStock: false,
+    lastDrop: ""
+  }
+];
+
+export default watches;
